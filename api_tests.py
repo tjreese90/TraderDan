72ccdd9ca4e8448873b7c3e53e50d1e4536ef7d5
Thomas <thomas18reese@gmail.com>
2023-02-22 18:52:13 -0600

    Creating the Candle Timing Class

--- api_tests.py
+++ api_tests.py
@@ -1,9 +1,11 @@
 from api.oanda_api import OandaApi
 from instrumentCollection.instrument_collection import instrumentCollection as ic
 import time
+from models.candle_timing import CandleTiming
 
 #TODO: Make that money!
 if __name__ == '__main__':
      api = OandaApi()
      ic.LoadInstruments("./data")
-     print(api.last_complete_candle("USD_JPY", granularity="M5"))
+     dd = api.last_complete_candle("USD_JPY", granularity="M5")
+     print(CandleTiming(dd))
