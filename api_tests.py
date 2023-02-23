35f383473fdde56fab7537ac680d8528625e2c59
Thomas <thomas18reese@gmail.com>
2023-02-22 18:45:39 -0600

    setting up last complete candle and getting granularity

--- api_tests.py
+++ api_tests.py
@@ -6,8 +6,4 @@ import time
 if __name__ == '__main__':
      api = OandaApi()
      ic.LoadInstruments("./data")
-     # trade_id = api.place_trade("GBP_JPY", 100, 1)
-     # print("opened:", trade_id)
-     # time.sleep(10)
-     # print(f"Closing {trade_id}", api.close_trade(trade_id))
-     [api.close_trade(x.id) for x in api.get_open_trades()] # type: ignore    
+     print(api.last_complete_candle("USD_JPY", granularity="M5"))
