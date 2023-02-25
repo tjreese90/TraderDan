99f59d63f1cb9907954b88b093c08cad31052ce8
Thomas <thomas18reese@gmail.com>
2023-02-24 19:52:21 -0600

    Setting up the bot live trading on 5 min candles

--- bot/trade_manger.py
+++ bot/trade_manger.py
@@ -4,8 +4,8 @@ from models.trade_decision import TradeDecision
 
 def trade_is_open(pair, api: OandaApi):
     
-    open_trades = api.get_open_trade()
-    
+    open_trades = api.get_open_trade() # type: ignore 
+   
     for ot in open_trades:
         if ot.instrumemt == pair:
             return ot
@@ -19,3 +19,17 @@ def place_trade(trade_decision: TradeDecision, api: OandaApi, log_message, log_e
     if ot is not None:
         log_message(f"Failed to place trade {trade_decision}, already open: {ot}", trade_decision.pair)
         return None
+    
+    trade_id = api.place_trade(
+        trade_decision.pair,
+        1000,
+        trade_decision.signal,
+        trade_decision.sl,
+        trade_decision.tp,
+    )
+    
+    if trade_id is not None:
+        log_error(f"Error palcing {trade_decision}")
+        log_message(f"Error placing {trade_decision}", trade_decision.pair)
+    else:
+        log_message(f"placed trade_id:{trade_id} for {trade_decision}", trade_decision.pair)
