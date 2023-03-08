5d6544caf43af40bfa46ecfbd58f9e7316854106
Thomas <thomas18reese@gmail.com>
2023-03-08 02:14:01 -0600

    Finishing setting up cacluating the units for trades of exptect dollar amount loss per pips in total

--- api_tests.py
+++ api_tests.py
@@ -2,6 +2,11 @@ from api.oanda_api import OandaApi
 from instrumentCollection.instrument_collection import instrumentCollection as ic
 import time
 from models.candle_timing import CandleTiming
+from bot.trade_risk_calculator import get_trade_units
+import constants.defs as defs
+
+def lm(msg, pair):
+     print(msg, pair)
 
 #TODO: Make that money!
 if __name__ == '__main__':
@@ -9,4 +14,5 @@ if __name__ == '__main__':
      ic.LoadInstruments("./data")
      # dd = api.last_complete_candle("USD_JPY", granularity="M5")
      # print(CandleTiming(dd))
-     print(api.get_prices(["GBP_JPY", "USD_JPY"]))
+     # print(api.get_prices(["GBP_JPY", "USD_JPY"]))
+     print(get_trade_units(api, "USD_JPY", defs.SELL, 0.4 , 60, lm))
