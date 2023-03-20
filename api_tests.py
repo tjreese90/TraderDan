19187dca8c8dcf0f006e55a40a7bb241261d9340
Thomas <thomas18reese@gmail.com>
2023-03-20 02:22:55 -0500

    Moving on to steaming Prices and Threading

--- api_tests.py
+++ api_tests.py
@@ -7,7 +7,8 @@ from bot.trade_risk_calculator import get_trade_units
 
 
 def lm(msg, pair):
-     print(msg, pair)
+     # print(msg, pair)
+     pass
 
 #TODO: Make that money!
 if __name__ == '__main__':
@@ -16,4 +17,6 @@ if __name__ == '__main__':
      # dd = api.last_complete_candle("USD_JPY", granularity="M5")
      # print(CandleTiming(dd))
      # print(api.get_prices(["GBP_JPY", "USD_JPY"]))
-     print(get_trade_units(api, "USD_JPY", defs.SELL, 0.4 , 60, lm))
+     print("GBP_JPY", get_trade_units(api, "GBP_JPY", defs.BUY, 0.4 , 20, lm))
+     print("AUD_NZD",get_trade_units(api, "AUD_NZD", defs.BUY, 0.004 , 20, lm))
+     print("USD_CAD",get_trade_units(api, "USD_CAD", defs.BUY, 0.004 , 20, lm))
