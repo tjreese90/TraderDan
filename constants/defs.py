4773517c128d1820d22b18aa21874bd34c7f6f34
Thomas <thomas18reese@gmail.com>
2023-03-20 04:27:35 -0500

    Setting Up Streaming of Live prices from API

--- constants/defs.py
+++ constants/defs.py
@@ -1,6 +1,12 @@
-API_KEY = "91e97c7b261216d6e6727272d86892a0-da24a042336ef33336d716c451ea6d36"
+API_KEY = "48407535e759c72dc2cb45329f0cc6a8-4151784dcad97aaa0ada707761d3c560"
 ACCOUNT_ID = "101-001-24345029-002"
 URL = "https://api-fxpractice.oanda.com/v3/"
+
+
+SECURE_HEADER = {
+            "Authorization": f"Bearer {API_KEY}",
+            "Content-Type" : "application/json"
+        }
 
 SELL = -1
 BUY = 1
--- main.py
+++ main.py
@@ -1,38 +1,10 @@
 from api.oanda_api import OandaApi
 from instrumentCollection.instrument_collection import instrumentCollection as ic
-from simulation.ma_cross import run_ma_sum
-from dateutil import parser
-from instrumentCollection.collect_data import run_collection
-from simulation.ema_macd_mp import run_ema_macd
-from simulation.ema_macd import run_ema_macd
+from api.steam_prices import steam_prices
+
 
 if __name__ == '__main__':
      api = OandaApi()
      ic.LoadInstruments("./data")
-     # run_collection(instrumentCollection, api)
-     # ic = instrumentCollection() # type: ignore 
-     run_ema_macd(ic)
+     steam_prices(['EUR_USD', 'AUD_NZD', 'GBP_JPY'])
 
-     # instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
-     # instrumentCollection.LoadInstruments("./data")
-     # instrumentCollection.PrintInstruments()
-
-    
-    
-    # dfr = parser.parse("2021-04-21T01:00:00Z")
-    # dto = parser.parse("2021-04-28T16:00:00Z")
-    
-    # df_candles = api.get_candles_df("EUR_USD",granularity="H1", date_f=dfr, date_t=dto)
-    
-    # print(df_candles.head()) # type: ignore
-    # print("----------------------------------")
-    # print(df_candles.tail()) # type: ignore
-    
-
-    
-    # # data = api.get_instruments()
-    # # [print(x['name']) for x in data] # type: ignore
-    
-    # # data1 = api.get_account_summary
-    # # print(data1)
-    # run_ma_sum(curr_list=["USD", "JPY","EUR","AUD"])
