de85d10618f40055560fdf6ba7d7c03178ce46f2
Thomas <thomas18reese@gmail.com>
2023-03-23 04:03:06 -0500

    Adding threading logic

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from api.oanda_api import OandaApi
 from instrumentCollection.instrument_collection import instrumentCollection as ic
 from api.steam_prices import steam_prices
+from stream_example.streamer import run_streamer
 
 
 if __name__ == '__main__':
      api = OandaApi()
      ic.LoadInstruments("./data")
-     steam_prices(['EUR_USD', 'AUD_NZD', 'GBP_JPY'])
-
+     # steam_prices(['EUR_USD', 'AUD_NZD', 'GBP_JPY'])
+     run_streamer()
