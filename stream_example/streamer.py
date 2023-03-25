a269e9fc83912ac1e25af35bd08a8c7b56fc16f0
Thomas <thomas18reese@gmail.com>
2023-03-25 02:18:29 -0500

    Completed basic setup of Streaming and Using live Prices

--- stream_example/streamer.py
+++ stream_example/streamer.py
@@ -2,6 +2,9 @@ import json
 import threading
 import time
 from stream_example.stream_prices import PriceStreamer
+from stream_example.stream_processor import PriceProcessor
+from queue import Queue
+from stream_example.stream_worker import WorkProcessor
 
 def load_settings():
     with open("./bot/settings.json", "r") as f:
@@ -15,6 +18,7 @@ def run_streamer():
     shared_prices = {}
     shared_prices_events = {}
     shared_prices_lock = threading.Lock()
+    work_queue = Queue()
     
     for p in settings['pairs'].keys():
         shared_prices_events[p] = threading.Event()
@@ -27,6 +31,16 @@ def run_streamer():
     threads.append(price_stream_t)
     price_stream_t.start()
     
+    worker_t = WorkProcessor(work_queue)
+    worker_t.daemon = True
+    threads.append(worker_t)
+    worker_t.start()
+    
+    for p in settings['pairs'].keys():
+        processing_t = PriceProcessor(shared_prices, shared_prices_lock, shared_prices_events, f"PriceProcessor_{p}", p , work_queue)
+        processing_t.daemon =True
+        threads.append(processing_t)
+        processing_t.start()
     
     # Tells you therad when to wait. KeyboardInterupt does not work here... We need the windows api libary to listen to correct handlers for windows. we instead use a whiile and a sleep. 
     
