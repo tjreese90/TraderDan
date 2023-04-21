d82d949371484e009dc4432ae4198fa447392399
Thomas <thomas18reese@gmail.com>
2023-04-20 23:17:25 -0500

    adding Mongo DB basics

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from api.oanda_api import OandaApi
 from instrumentCollection.instrument_collection import instrumentCollection as ic
 from stream_example.streamer import run_streamer
-
+from DB.db import DataDB
 
 if __name__ == '__main__':
-     api = OandaApi()
-     ic.LoadInstruments("./data")
-     # steam_prices(['EUR_USD', 'AUD_NZD', 'GBP_JPY'])
-     run_streamer()
+    #  api = OandaApi()
+    #  ic.LoadInstruments("./data")
+    #  # steam_prices(['EUR_USD', 'AUD_NZD', 'GBP_JPY'])
+    #  run_streamer()
+    d = DataDB()
+    d.test_connection()
