1fba681da034d3f09f2c1dcbd22bd18e0d0f4aa5
Thomas Reese <thomas18reese@gmail.com>
2023-07-02 02:39:55 -0500

    Fixing React Server and adding accocunt end point API back

--- forex-dash/src/app/api.js
+++ forex-dash/src/app/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_URL;
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const response = (resp) => resp.data;
 
 const request = {
-    get: (url) => axios.get(url).then(response),
+    get: (url) => axios.get(url).then(response)
 }
 
 const endPoints = {
