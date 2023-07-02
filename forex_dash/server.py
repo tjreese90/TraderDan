728d60f98f0f58a05dc45824009b78debf3c5542
Thomas Reese <thomas18reese@gmail.com>
2023-07-02 04:27:22 -0500

    Adding Python dev server start automatically with yarn start launch and fixing some api calls and folder structure to start back on project course now

---
+++ forex_dash/server.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from scraping.bloomberg_com import bloomberg_com
+from scraping.investing_com import get_pair
+from api.oanda_api import OandaApi
+
+app = Flask(__name__)
+CORS(app)
+# NOTE: We must start the DB Server or Back-end Server and then the yarn start
+
+@app.route('/api/test')
+def test():
+    return jsonify(dict(message='hello world'))
+
+@app.route('/api/account')
+def account():
+    return jsonify(OandaApi().get_account_summary())
+
+@app.route('/api/headlines')
+def headlines():
+    return jsonify(bloomberg_com())
+
+@app.route('/api/technicals/<pair>/<tfs>')
+def technicals(pair, tfs):
+    data = get_pair(pair, tfs)
+    if data is None:
+        return jsonify(dict(message='No data found'))
+    else:
+        return jsonify(data)
+    
+@app.route('/api/prices/<pair>/<granularity>/<count>')
+def prices(pair, granularity, count):
+    data = OandaApi().web_api_candles(pair, granularity, count)
+    if data is None:
+        return jsonify(dict(message='No data found'))
+    else:
+        return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forex_dash/src/pages/Dashboard.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import endPoints from '../app/api';
+import { COUNTS } from '../app/data';
+import Button from '../components/Button';
+import PriceChart from '../components/PriceChart';
+import Select from '../components/Select';
+import Technicals from '../components/Technicals';
+import TitleHead from '../components/TitleHead';
+
+function Dashboard() {
+
+  const [ selectedPair, setSelectedPair ] = useState(null);
+  const [ selectedGran, setSelectedGran ] = useState(null);
+  const [ technicalsData, setTechnicalsData ] = useState(null);
+  const [ priceData, setPriceData ] = useState(null);
+  const [ selectedCount, setSelectedCount ] = useState(COUNTS[0].value);
+  const [ options, setOptions ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    loadOptions();
+  }, []);
+
+  const handleCountChange = (count) => {
+    setSelectedCount(count);
+    loadPrices(count);
+  }
+
+  const loadOptions = async () => {
+    const data = await endPoints.options();
+    setOptions(data);
+    setSelectedGran(data.granularities[0].value);
+    setSelectedPair(data.pairs[0].value);
+    setLoading(false);
+  }
+
+  const loadPrices = async (count) => {
+    const data = await endPoints.prices(selectedPair, selectedGran, count);
+    setPriceData(data);
+  }
+
+  const loadTechnicals = async () => {
+    const data = await endPoints.technicals(selectedPair, selectedGran);
+    setTechnicalsData(data);
+    loadPrices(selectedCount);
+  }
+
+  if(loading === true) return <h1>Loading...</h1>
+
+  return (
+    <div>
+      <TitleHead title="Options" />
+      <div className="segment options">
+        <Select 
+          name="Currency"
+          title="Select currency"
+          options={options.pairs}
+          defaultValue={selectedPair}
+          onSelected={setSelectedPair}
+        />
+        <Select 
+          name="Granularity"
+          title="Select granularity"
+          options={options.granularities}
+          defaultValue={selectedGran}
+          onSelected={setSelectedGran}
+        />
+        <Button text="Load" handleClick={() => loadTechnicals()} />
+      </div>
+      <TitleHead title="Technicals" />
+      { technicalsData && <Technicals data={technicalsData} /> }
+      <TitleHead title="Price Chart" />
+      { priceData && <PriceChart
+        selectedCount={selectedCount}
+        selectedPair={selectedPair}
+        selectedGranularity={selectedGran}
+        handleCountChange={handleCountChange}
+        priceData={priceData}
+      />}
+    </div>
+  )
+}
+
+export default Dashboard
