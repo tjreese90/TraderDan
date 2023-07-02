9e826bd408a656fa8cf2dcf51704239eebdb6ee3
Thomas Reese <thomas18reese@gmail.com>
2023-07-02 16:12:44 -0500

    adding database rendering

--- run_bot.py
+++ run_bot.py
@@ -2,6 +2,6 @@ from bot.bot import Bot
 from instrumentCollection.instrument_collection import instrumentCollection as ic
 
 if __name__ == "__main__":
-    ic.LoadInstruments("./data")
+    ic.LoadInstrumentsDB()
     b = Bot()
     b.run()
